e19bd01d97c1f3354b5c897101e5e661b55c95bc
James <1146556298@qq.com>
2018-02-12 19:47:56 +0800

    添加 settings

---
+++ spider/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+""" 
+@author:James
+Created on:18-2-12 19:43
+"""
---
+++ spider/settings.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+""" 
+@author:James
+Created on:18-2-12 19:43
+"""
+# 搜索结果页
+URL_RESULT = 'http://sou.zhaopin.com/jobs/searchresult.ashx'
+
+# 搜索请求参数名
+
+# 1.职位关键字
+KEY_KEYWORD = 'kw'
+
+# 2.地域范围
+KEY_ADDRESS = 'jl'
+
+# 3.页码
+KYE_PAGENUM = 'p'
